122a0d5f01ba87baa08f298acef77e61f89dab2e
RafaelTSilva13 <rafael_teixeira_1987@hotmail.com>
2023-10-11 23:39:56 -0300

    Revert "alteracao 1"
    This reverts commit c4e44c4eea3ab1a4615490062d048a8c0a61fbae.

--- Teste/Controllers/WeatherForecastController.cs
+++ Teste/Controllers/WeatherForecastController.cs
@@ -38,8 +38,8 @@ namespace Teste.Controllers
 
         public string GetTeste()
         {
-            DateTime teste = DateTime.Now;
-            return teste.ToString("yyyy-MM-dd");
+            int teste = 123;
+            return teste.ToString();
         }
 
         public string GetTeste2()
